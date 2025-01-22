a21aedf3c51d746cab2b4515468bbb7924ad293e
Ujvari Gyorgy <ujvari.gyorgy@gmail.com>
2025-01-22 15:32:00 +0200

    load documents to menu

--- notion-ai/components/SideBar.tsx
+++ notion-ai/components/SideBar.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
 import NewDocumentButton from "./NewDocumentButton";
 import {
   Sheet,
@@ -9,11 +12,81 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { MenuIcon } from "lucide-react";
+import { adminDb } from "@/firebase-admin";
+import { useUser } from "@clerk/nextjs";
+import {
+  DocumentData,
+  collectionGroup,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "@/firebase";
+
+interface RoomDocument extends DocumentData {
+  createdAt: string;
+  role: "owner" | "editor";
+  roomId: string;
+  userId: string;
+}
 
 export default function SideBar() {
+  const { user }: any = useUser();
+  const [groupedData, setGroupedData] = React.useState<{
+    owner: RoomDocument[];
+    editor: RoomDocument[];
+  }>({
+    owner: [],
+    editor: [],
+  });
+  const [data, loading, error] = useCollection(
+    user &&
+      query(
+        collectionGroup(db, "rooms"),
+        where("userId", "==", user.emailAddresses[0].toString()) // Fix here
+      )
+  );
+  useEffect(() => {
+    if (!data) return;
+
+    const groups = data.docs.reduce<{
+      owner: RoomDocument[];
+      editor: RoomDocument[];
+    }>(
+      (acc, curr) => {
+        const room = curr.data() as RoomDocument;
+        if (room.role === "owner") {
+          acc.owner.push({ id: curr.id, ...room });
+        } else {
+          acc.editor.push({ id: curr.id, ...room });
+        }
+        return acc;
+      },
+      {
+        owner: [],
+        editor: [],
+      }
+    );
+    setGroupedData(groups);
+  }, [data]);
+
   const menuOptions = (
     <>
       <NewDocumentButton />
+      {groupedData.owner.length === 0 ? (
+        <h2 className="text-center">No Documents</h2>
+      ) : (
+        <div className="flex flex-col gap-2">
+          <h2 className="text-center">Documents</h2>
+          {groupedData.owner.map((room) => (
+            <div
+              key={room.roomId}
+              className="flex flex-col gap-2 p-2 border rounded-lg"
+            >
+              <h3>{room.roomId}</h3>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 
